ebd4866dd287bcda7d4913410288d1305abadc64
Leo Kirasic <kirasicleo@gmail.com>
2022-02-10 18:34:54 +0100

    Add shopping cart image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 function Header() {
-  return <h1 className="text-xl">GOLF</h1>;
+  return <div className=''>
+    <div>GOLF</div>
+    <img className=' w-8 absolute top-1 right-3' src={require('../assets/header/shopping-bag.png')} alt="" />
+  </div>;
 }
 
 export default Header;
